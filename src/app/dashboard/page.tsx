8c471fcfa14d1b7ee16e32ab52277e44d8bbf145
Wupxy <muhammad.122140193@student.itera.ac.id>
2023-10-29 18:56:51 +0700

    set-up:initialize project

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,26 +1,17 @@
-import Dashboard from '@/components/Dashboard'
-import { db } from '@/db'
-import { getUserSubscriptionPlan } from '@/lib/stripe'
-import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
-import { redirect } from 'next/navigation'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
-const Page = async () => {
-  const { getUser } = getKindeServerSession()
-  const user = getUser()
+const Page = () => {
+  const { getUser } = getKindeServerSession();
+  const user = getUser();
 
-  if (!user || !user.id) redirect('/auth-callback?origin=dashboard')
+  return (
+    <div>
+      <h1>dashboard</h1>
+      <h1>{user.email}</h1>
+    </div>
+  );
+};
 
-  const dbUser = await db.user.findFirst({
-    where: {
-      id: user.id
-    }
-  })
-
-  if(!dbUser) redirect('/auth-callback?origin=dashboard')
-
-  const subscriptionPlan = await getUserSubscriptionPlan()
-
-  return <Dashboard subscriptionPlan={subscriptionPlan} />
-}
-
-export default Page
+export default Page;
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,32 +1,29 @@
-import { type ClassValue, clsx } from 'clsx'
-import { Metadata } from 'next'
-import { twMerge } from 'tailwind-merge'
+import { type ClassValue, clsx } from 'clsx';
+import { Metadata } from 'next';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
 export function absoluteUrl(path: string) {
-  if (typeof window !== 'undefined') return path
-  if (process.env.VERCEL_URL)
-    return `https://${process.env.VERCEL_URL}${path}`
-  return `http://localhost:${
-    process.env.PORT ?? 3000
-  }${path}`
+  if (typeof window !== 'undefined') return path;
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}${path}`;
+  return `http://localhost:${process.env.PORT ?? 3000}${path}`;
 }
 
 export function constructMetadata({
-  title = "Quill - the SaaS for students",
-  description = "Quill is an open-source software to make chatting to your PDF files easy.",
-  image = "/thumbnail.png",
-  icons = "/favicon.ico",
-  noIndex = false
+  title = 'Quill - the SaaS for students',
+  description = 'Quill is an open-source software to make chatting to your PDF files easy.',
+  image = '/thumbnail.png',
+  icons = '/favicon.ico',
+  noIndex = false,
 }: {
-  title?: string
-  description?: string
-  image?: string
-  icons?: string
-  noIndex?: boolean
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: string;
+  noIndex?: boolean;
 } = {}): Metadata {
   return {
     title,
@@ -36,16 +33,16 @@ export function constructMetadata({
       description,
       images: [
         {
-          url: image
-        }
-      ]
+          url: image,
+        },
+      ],
     },
     twitter: {
-      card: "summary_large_image",
+      card: 'summary_large_image',
       title,
       description,
       images: [image],
-      creator: "@joshtriedcoding"
+      creator: '@joshtriedcoding',
     },
     icons,
     metadataBase: new URL('https://quill-jet.vercel.app'),
@@ -53,8 +50,8 @@ export function constructMetadata({
     ...(noIndex && {
       robots: {
         index: false,
-        follow: false
-      }
-    })
-  }
+        follow: false,
+      },
+    }),
+  };
 }
